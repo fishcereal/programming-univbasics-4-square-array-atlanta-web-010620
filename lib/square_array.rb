32714a75b78a0a7a7e97ef9b0f0d0d93a8377d25
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-04 16:46:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
   counter = 0 
-  new_array = []
+  # new_array = []
   while counter < array.length do 
-    new_array.push(array[counter]**array[counter])
+    # new_array.push(array[counter]**array[counter])
+    array[counter] = array[counter]**array[counter]
     counter+=1 
   end
-  return new_array
+  
 end
